b9e49264f5bbff51d07c7d0202d430f87f28623a
selaguardia <selaguardia91@gmail.com>
2021-08-18 12:11:14 -0700

    stepped away. Need to add view templates

--- albums_app/views.py
+++ albums_app/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 # View class to hangle requests
 from django.views import View
 # to hanfle sending a type of requests
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
-class Home(View):
-  def get(self, request):
-    return HttpResponse('Albums Homepage')
+class Home(TemplateView):
+  template_name = 'home.html'
 
-class About(View):
-  def get(self, request):
-    return HttpResponse('Albums About page')
+class About(TemplateView):
+  template_name = 'about.html'
