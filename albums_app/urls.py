3881c3939c63b213db1bd032a14fb68b4379845d
selaguardia <selaguardia91@gmail.com>
2021-08-18 19:11:10 -0700

    adds create page and fixed home page

--- albums_app/urls.py
+++ albums_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import Home, About
+from .views import Home, About, AlbumCreate
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
   path('about/', About.as_view(), name='about'),
+  path('new/', AlbumCreate.as_view(), name='album_create'),
 ]
--- albums_app/views.py
+++ albums_app/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
-# View class to hangle requests
+# View class to handle requests
 from django.views import View
 # to hanfle sending a type of requests
 # from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+
+from .models import Album
 
 # Create your views here.
 class Home(TemplateView):
   template_name = 'home.html'
 
+  def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["albums"] = Album.objects.all()
+        return context
+
 class About(TemplateView):
   template_name = 'about.html'
 
+# GET POST /album
+class AlbumCreate(CreateView):
+  model = Album
+  fields = ['title', 'artist', 'release_date', 'cover_img', 'description']
+  template_name = 'album_create.html'
